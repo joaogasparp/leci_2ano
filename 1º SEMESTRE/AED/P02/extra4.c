9bcfd87cd92c62ab44567299d36d1ef7149fb133
João Gaspar <joaogaspar003@gmail.com>
2022-10-08 18:13:30 +0100

    aed

--- 1º SEMESTRE/AED/P02/extra4.c
+++ 1º SEMESTRE/AED/P02/extra4.c
@@ -2,16 +2,22 @@
 #include <assert.h>
 #include <stdlib.h>
 
-void display(int *a, size_t size){
+int* display(int *a, size_t size){
+
+    if(size < 0){
+        return NULL;
+    }
+
     printf("Array: [ ");
     for(int i = 0; i < size-1; i++){
         printf("%d, ", a[i]);
     }
 
     printf("%d ]\n", a[size-1]);
+    return 0;
 }
 
-int* read(size_t *size){
+int* read(size_t *size, int num){
     // pedir o n de elementos
     // alocar membros para o array e verificar se a alocação foi válida!!  usar: int* novo = malloc(n * sizeof(int)); if(novo == NULL ){*size = 0; return NULL;}
     // preenche o array elemento a elemento
@@ -20,36 +26,38 @@ int* read(size_t *size){
 
     assert(size != NULL);
 
-    size_t n;
-    printf("Array size (> 0): ");
-    scanf("%lu", &n);
-    assert(n > 0);
-
-    int* array = malloc(n * sizeof(int));
+    int* array = malloc(*size * sizeof(int));
     if(array == NULL){
         *size = 0;
         return NULL;
     }
 
-    for(size_t i = 0; i < n; i++){
-        printf("A[%lu] = ", i);
+    for(size_t i = 0; i < *size; i++){
+        printf("A_%d[%lu] = ", num, i);
         scanf("%d", &array[i]);
     }
 
-    *size = n;
-
-    display(array, n);
     return array;
 }
 
 int* append(int *a1, size_t size1, int *a2, size_t size2){
     int c[size1 + size2];
 
+    if(size1 < 0 || size2 < 0){
+        return NULL;
+    }
+
     size_t size3 = size1 + size2;
     for(int i = 0; i < size1; i++)
        c[i] = a1[i];
     for(int i = 0; i < size2; i++)     
         c[i + size1] =a2[i];
+
+    if(c == NULL){
+        size1 = 0;
+        size2 = 0;
+        return NULL;
+    }
 
     printf("Merged ");
 
@@ -59,18 +67,24 @@ int* append(int *a1, size_t size1, int *a2, size_t size2){
 }
 
 int main(void){
-    size_t size1 = 3;
-    int a1[3] = {1, 2, 3};
-    display(a1, size1);
+    // size_t size1 = 3;
+    // int a1[3] = {1, 2, 3};
+    // display(a1, size1);
 
-    size_t size2 = 4;
-    int a2[4] = {4, 5, 6, 7};
-    display(a2, size2);
+    // size_t size2 = 4;
+    // int a2[4] = {4, 5, 6, 7};
+    // display(a2, size2);
 
-    append(a1, size1, a2, size2);
+    size_t size3;
+    printf("Array_1 size (> 0): ");
+    scanf("%lu", &size3);
+    assert(size3 > 0);
+    size_t size4;
+    printf("Array_2 size (> 0): ");
+    scanf("%lu", &size4);
+    assert(size4 > 0);
 
-    size_t size;
-    read(&size);
+    append(read(&size3, 1), size3, read(&size4, 2), size4);    
     
     return 0;
 }
--- 1º SEMESTRE/AED/P02/extra5.c
+++ 1º SEMESTRE/AED/P02/extra5.c
@@ -2,6 +2,48 @@
 #include <assert.h>
 #include <stdlib.h>
 
+void DisplayPol(double* coef, unsigned int degree) {
+
+    printf("Pol(x) = ");
+
+    int num = degree;
+
+    for(int i = 0; i < num; i++){
+        printf("%0.3f * x^%u + ", coef[i], degree--);
+    }
+
+    printf("%0.3f\n", coef[degree]);
+}
+
+int* read(unsigned int *degree){
+
+    assert(degree != NULL);
+    
+    printf("Write down your coefficients knowing that p[0] is the coefficient of the largest degree term and p[n] is the coefficient of the zero-degree term. (n it's the degree of the polynom)");
+    printf("What's the degree? ");
+    scanf("%d", degree);
+    
+    int* polynom = malloc(*degree * sizeof(int));
+    if(polynom == NULL){
+        *degree = 0;
+        return NULL;
+    }
+
+    for(size_t i = 0; i < *degree; i++){
+        printf("p[%lu] = ", i);
+        scanf("%d", &polynom[i]);
+    }
+
+    return polynom;
+}
+
 int main(void){
-    printf("Hello Word!\n");
+
+    // unsigned int degree = 4;
+    // double p[5] = {1, 4, 1, 4, 0};
+    // DisplayPol(p, degree);
+
+    int degree;
+    read(&degree);
+
 }
