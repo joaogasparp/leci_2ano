cd6e2b3e5de7cccc84b0de839a3cc33c487d0078
João Gaspar <j.gaspar@ua.pt>
2022-12-07 22:42:41 +0000

    aed

--- 1º SEMESTRE/AED/TA1/Gráficos/matlab.m
+++ 1º SEMESTRE/AED/TA1/Gráficos/matlab.m
@@ -13,7 +13,7 @@ h1.Color = 'r';
 xlabel('n','FontSize',10); % legendar eixo dos x
 ylabel('cpu-time','FontSize',10); % legendar eixo dos y
 title('Solução Fornecida - Nº Mec. 107708'); % dar título
-xlim([0 50]); % limitar eixo dos x
+xlim([38 50]); % limitar eixo dos x
 ylim([0 1200]); % limitar eixo dos y
 p1 = polyfit(md1,md2,4); % encontrar fórmula com 4 coeficientes
 val1 = polyval(p1,800); % calcular valor para 800
@@ -23,13 +23,14 @@ val1 = polyval(p1,800); % calcular valor para 800
 n = 38:1:50;
 p1 = 0.179683515012509*n.^4-29.874161695810340*n.^3+(1.862710731929421e+03)*n.^2-(5.161081233702307e+04)*n+5.360544850625902e+05;
 figure(2);
-plot(md1,md2,'Marker','.','LineStyle','none','Color','b'); % criar plot
+plot(n,p1,'Marker','.','LineStyle','none','Color','b'); % criar plot
 h1 = lsline; % reta de ajuste
 h1.Color = 'r';
 xlabel('n','FontSize',10);
 ylabel('cpu-time','FontSize',10);
-ylim([0 1200])
 title('Prova da Equação')
+xlim([38 50]); % limitar eixo dos x
+ylim([0 1200]); % limitar eixo dos y
 
 %%%%%% solucao 1
 
