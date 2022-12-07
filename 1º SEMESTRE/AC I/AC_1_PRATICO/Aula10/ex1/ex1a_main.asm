d9315568a9c4df26a1cddcaa65d95df8e1ba704c
João Gaspar <j.gaspar@ua.pt>
2022-12-07 14:51:33 +0000

    ac1

---
+++ 1º SEMESTRE/AC I/AC_1_PRATICO/Aula10/ex1/ex1a_main.asm
@@ -0,0 +1,21 @@
+	.data
+fnum:	.float 2.0
+	.eqv print_float, 2
+	.text
+	.globl main
+
+main:	addiu $sp,$sp,-4
+	sw $ra,0($sp)
+	
+	li $a0,4
+	l.s $f12,fnum
+	jal xtoy
+	
+	li $v0, print_float
+	mov.s $f12,$f0
+	syscall
+	
+	lw $ra,0($sp)
+	addiu $sp,$sp,4
+	jr $ra
+	
---
+++ 1º SEMESTRE/AC I/AC_1_PRATICO/Aula10/ex1/ex1a_xtoy.asm
@@ -0,0 +1,38 @@
+# Mapa de registos:
+# x: 
+# y: 
+# i: $s1
+# result: 
+# abs(y): 
+	.data
+one:	.float 1.0
+	.text
+	.globl xtoy
+	
+xtoy:	addiu $sp,$sp,-12
+	sw $ra,0($sp)
+	sw $s0,4($sp)
+	sw $s1,8($sp)
+	
+	move $s0,$a0
+	li $s1,0
+	l.s $f4,one
+	
+for:	jal aabs
+	bge $s1,$v0,endf
+	blez $s0,else
+	mul.s $f4,$f4,$f12
+	j eif
+	
+else:	div.s $f4,$f4,$f12
+
+eif:	addiu $s1,$s1,1
+	move $a0,$s0
+	j for
+	
+endf:	mov.s $f0,$f4
+	lw $s1,8($sp)
+	lw $s0,4($sp)
+	lw $ra,0($sp)
+	addiu $sp,$sp,12
+	jr $ra
