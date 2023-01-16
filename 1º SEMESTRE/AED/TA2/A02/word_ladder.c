0bd82e01d43a48556024f5b49676f2ac7608e1a6
João Gaspar <j.gaspar@ua.pt>
2023-01-16 23:25:47 +0000

    aed

--- 1º SEMESTRE/AED/TA2/A02/word_ladder.c
+++ 1º SEMESTRE/AED/TA2/A02/word_ladder.c
@@ -8,13 +8,13 @@
 //
 // Do as much as you can
 //   1) MANDATORY: complete the hash table code
-//      *) hash_table_create          DONE
-//      *) hash_table_grow            
-//      *) hash_table_free            DONE
-//      *) find_word
+//      *) hash_table_create                                                    DONE
+//      *) hash_table_grow                                                      DONE
+//      *) hash_table_free                                                      DONE
+//      *) find_word                                                            DONE
 //      +) add code to get some statistical data about the hash table
 //   2) HIGHLY RECOMMENDED: build the graph (including union-find data) -- use the similar_words function...
-//      *) find_representative        DONE
+//      *) find_representative                                                  DONE
 //      *) add_edge
 //   3) RECOMMENDED: implement breadth-first search in the graph
 //      *) breadh_first_search
