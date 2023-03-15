c2cb15d603a0771466988629834aaad9b2da567a
João Gaspar <joaogaspar003@gmail.com>
2023-03-15 12:54:53 +0000

    ac2

--- 2º SEMESTRE/AC II/P/aula04/parte02/ex1.c
+++ 2º SEMESTRE/AC II/P/aula04/parte02/ex1.c
@@ -1,51 +1,48 @@
 #include <detpic32.h>
 
-void delay(unsigned int ms)
-{
-    resetCoreTimer();
-    while(readCoreTimer() < 20000 * ms);
-}
-
 int main(void)
 {
-    LATBbits.LATB8 = 0;
-    TRISBbits.TRISB8 = 0;
-    LATBbits.LATB9 = 0;
-    TRISBbits.TRISB9 = 0;
-    LATBbits.LATB10 = 0;
-    TRISBbits.TRISB10 = 0;
-    LATBbits.LATB11 = 0;
-    TRISBbits.TRISB11 = 0;
-    LATBbits.LATB12 = 0;
-    TRISBbits.TRISB12 = 0;
-    LATBbits.LATB13 = 0;
-    TRISBbits.TRISB13 = 0;
-    LATBbits.LATB14 = 0;
-    TRISBbits.TRISB14 = 0;
-    LATDbits.LATD5 = 0;
-    TRISDbits.TRISD5 = 0;
-    LATDbits.LATD6 = 0;
-    TRISDbits.TRISD6 = 0;
+    LATD = (LATD & 0xFF9F);
+    LATB = (LATB & 0x80FF);
+
+    TRISD = (TRISD & 0xFF9F);
+    TRISB = (TRISB & 0x80FF);
 
     LATDbits.LATD5 = 1;
-    ATDbits.LATD6 = 0;
+    LATDbits.LATD6 = 0;
 
     while(1)
     {
         char c = getChar();
-        if((c >= 'a' || c <= 'g') && (c >= 'A' || c <= 'G'))
+        switch(c)
         {
-            switch(c)
-            {
-                case 'a':
-                case 'A':
-                    LATB = 
-                    break;
-                
-            }
+            case 'a':
+            case 'A':
+                LATB = 0x7700;
+                break;
+            case 'b':
+            case 'B':
+                LATB = 0x7C00;
+                break;
+            case 'c':
+            case 'C':
+                LATB = 0x3900;
+                break;
+            case 'd':
+            case 'D':
+                LATB = 0x5E00;
+                break;
+            case 'e':
+            case 'E':
+                LATB = 0x7900;
+                break;
+            case 'f':
+            case 'F':
+                LATB = 0x7100;
+                break;
+            default:
+                break;            
         }
-
-        
     }
-    
+    return 0;    
 }
