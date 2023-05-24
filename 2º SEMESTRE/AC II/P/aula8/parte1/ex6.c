93d9a407e7473fb65e1890116784be1e8daf9f0a
João Gaspar <joaogaspar003@gmail.com>
2023-05-24 12:50:36 +0100

    ac2

---
+++ 2º SEMESTRE/AC II/P/aula8/parte1/ex6.c
@@ -0,0 +1,48 @@
+#include <detpic32.h>
+
+int main(void)
+{
+    // Configure Timers T1 and T3 with interrupts enabled
+    // T1
+    T1CONbits.TCKPS = 6;
+    PR1 = 62499;
+    TMR1 = 0;
+    T1CONbits.TON = 1;
+    // T3
+    T3CONbits.TCKPS = 4;
+    PR3 = 49999;
+    TMR3 = 0;
+    T3CONbits.TON = 1;
+    // Reset T1IF and T3IF flags
+    // T1
+    IPC1bits.T1IP = 1;
+    IEC0bits.T1IE = 1;
+    IFS0bits.T1IF = 0;
+    // T3
+    IPC3bits.T3IP = 2;
+    IEC0bits.T3IE = 1;
+    IFS0bits.T3IF = 0;
+
+    TRISEbits.TRISE0 = 0;
+    TRISEbits.TRISE2 = 0;
+    LATEbits.LATE0 = 0;
+    LATEbits.LATE2 = 0;
+
+    EnableInterrupts(); // Global Interrupt Enable
+    while(1);
+    return 0;
+}
+
+void _int_(4) isr_T1(void)
+{
+    LATEbits.LATE0 = !LATEbits.LATE0;
+    // Reset T1IF flag
+    IFS0bits.T1IF = 0;
+}
+
+void _int_(12) isr_T3(void)
+{
+    LATEbits.LATE2 = !LATEbits.LATE2;
+    // Reset T3IF flag  
+    IFS0bits.T3IF = 0; 
+}
